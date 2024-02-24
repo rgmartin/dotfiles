bd119116ceae7b8b7c718ee8e6cec0ba38e36d6e
rubertmartinpardo <rubert.martin@talent.com>
2024-02-24 11:28:44 -0500

    changed hammerspoon

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc  zshrc config gitconfig"    # list of files/folders to symlink in homedir
+files="vimrc zshrc config gitconfig hammerspoon"    # list of files/folders to symlink in homedir
 
 ##########
 
