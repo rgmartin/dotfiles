7233c534f7cab69dc76b3a65aad8a66880eee579
Rubert <rmartinpardo@gmail.com>
2024-08-28 12:26:10 -0400

    updated nvim configuration to that of josean

--- Mackup/.config/nvim/init.lua
+++ Mackup/.config/nvim/init.lua
@@ -1,17 +1,2 @@
-
--- Download lazyvim from github if not present
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("vim-options")
-require("lazy").setup("plugins")
+require("josean.core")
+require("josean.lazy")
